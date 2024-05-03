97091fdf1d3c9611a1c836e8898c04a96009bbae
Hunter Greer <huntergreer03@gmail.com>
2024-05-03 11:22:11 -0700

    serial sending fuctionality

--- CommandController/commandController.py
+++ CommandController/commandController.py
@@ -7,7 +7,13 @@ ser = serial.Serial('COM5', 1200)
 time.sleep(2)
 ser.reset_input_buffer()
 
+while(True):
+    print("Enter your command. EXIT to exit")
+    command = input()
 
+    if(command == "EXIT"):
+        break
 
-ser.write(b'G')
+    ser.write(command.encode("utf-8"))
+
 ser.close()
