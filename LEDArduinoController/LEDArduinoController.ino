e03116d23067dd34f115d84d06294ae31564d4c6
Hunter Greer <huntergreer03@gmail.com>
2024-05-03 11:12:54 -0700

    Added basic command receiving logic

--- LEDArduinoController/LEDArduinoController.ino
+++ LEDArduinoController/LEDArduinoController.ino
@@ -8,16 +8,23 @@ Author: Hunter Greer
 #include <FastLED.h>
 #include <string.h>
 
+
 #define NUM_LEDS 300
 #define LED_PIN 13
 
 CRGB leds[NUM_LEDS];
 
+//command attributes
+const byte numChars = 32;
+char receivedChars[numChars];  // an array to store the received data
+
+boolean newData = false;
+
 //strip appearence properties
 uint8_t brightness = 5;
 
 //i think rgb is a keyword so "RGB" was not working properly
-struct RGBs{
+struct RGBs {
   uint8_t red;
   uint8_t green;
   uint8_t blue;
@@ -34,7 +41,7 @@ void setup() {
   FastLED.setBrightness(brightness);
 
   //loop until serial connection is ready
-  while(!Serial) {}
+  while (!Serial) {}
 }
 
 void loop() {
@@ -44,30 +51,71 @@ void loop() {
   FastLED.show();
 }
 
+/*
+* Supported Commands for Serial Input
+* "RGB,R,G,B" for setting the strip to a whole color where R, G, and B are the number values that correspond to red, green, and blue color values respectively that range from 0-255
+* "BR,V" for changing the brightness of the strip to the specified V value
+*/
+void readSerial() {
 
-void readSerial(){
-  if(Serial.available() > 0){
+  while (Serial.available() > 0) {
+    int commandArrLength = 4;
+    String commandArr[commandArrLength];
 
-    //read and print out the command
+    //read command
     String command = Serial.readString();
+    const int cmdLength = command.length();
 
-    Serial.print(command);
+    //convert command to char array
+    char* char_array = new char[cmdLength]; 
+    strcpy(char_array, command.c_str());
+
+    //split command into parts seperated by ","
+    char * p;
+    p = strtok(char_array, ","); 
+
+    //loop through and add the split elements to commandArr
+    int i = 0;
+    while (p != NULL) {
+
+      commandArr[i] = p;
+      p = strtok(NULL, ",");
+      i++;
+    }
+
+    //loop through and print out commandArr to make sure it has the elements we need
+    /*for(int i = 0; i < commandArrLength; i++){
+      Serial.println(commandArr[i]);
+    }
+    */
+    Serial.print(commandArr[0]);
+    if(commandArr[0].equals("RGB")){
+      int val1 = commandArr[1].toInt();
+      int val2 = commandArr[2].toInt();
+      int val3 = commandArr[3].toInt();
+      remoteChangeColorRGB(val1, val2, val3);
+    }else if(commandArr[0].equals("BR")){
+      int val1 = commandArr[1].toInt();
+      changeBrightness(val1);
+    }
 
 
-    //if(values[0] == "WRGB"){
-      //remoteChangeColorRGB(values[1], values[2], values[3]);
-    //}
 
+    delete[] char_array;
   }
 }
 
 
 
 /*
-Functions for use from server
+LED Functions
 */
 
-//not implemented -- currently set to green/blue
-void remoteChangeColorRGB(int red, int green, int blue){
+//change the whole strip to the specified red green and blue values
+void remoteChangeColorRGB(int red, int green, int blue) {
   fill_solid(leds, NUM_LEDS, CRGB(red, green, blue));
+}
+
+void changeBrightness(int brightness){
+  FastLED.setBrightness(brightness);
 }
