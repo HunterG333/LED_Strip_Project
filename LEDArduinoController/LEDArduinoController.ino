382a737e8ca064fc7dfa7fa83f94ad9b7dbaa152
Hunter Greer <huntergreer03@gmail.com>
2024-05-06 08:53:47 -0700

    Adder error code print out to LEDs

--- LEDArduinoController/LEDArduinoController.ino
+++ LEDArduinoController/LEDArduinoController.ino
@@ -67,12 +67,12 @@ void readSerial() {
     const int cmdLength = command.length();
 
     //convert command to char array
-    char* char_array = new char[cmdLength]; 
+    char* char_array = new char[cmdLength];
     strcpy(char_array, command.c_str());
 
     //split command into parts seperated by ","
-    char * p;
-    p = strtok(char_array, ","); 
+    char* p;
+    p = strtok(char_array, ",");
 
     //loop through and add the split elements to commandArr
     int i = 0;
@@ -89,12 +89,12 @@ void readSerial() {
     }
     */
     Serial.print(commandArr[0]);
-    if(commandArr[0].equals("RGB")){
+    if (commandArr[0].equals("RGB")) {
       int val1 = commandArr[1].toInt();
       int val2 = commandArr[2].toInt();
       int val3 = commandArr[3].toInt();
       remoteChangeColorRGB(val1, val2, val3);
-    }else if(commandArr[0].equals("BR")){
+    } else if (commandArr[0].equals("BR")) {
       int val1 = commandArr[1].toInt();
       changeBrightness(val1);
     }
@@ -116,6 +116,39 @@ void remoteChangeColorRGB(int red, int green, int blue) {
   fill_solid(leds, NUM_LEDS, CRGB(red, green, blue));
 }
 
-void changeBrightness(int brightness){
+void changeBrightness(int brightness) {
   FastLED.setBrightness(brightness);
+}
+
+//displays a binary error code 0 = blue, 1 = red onnto the led strip. The binary string length must not be higher than the number of LEDs
+void displayError(int errorNumber) {
+
+  //base case
+  if (errorNumber == 0) {
+    leds[0] = CRGB::Blue;
+    return;
+  }
+
+  Serial.print(errorNumber);
+
+  // array to store binary number
+  int binaryNum[32];
+
+  // counter for binary array
+  int i = 0;
+  while (errorNumber > 0) {
+
+    // storing remainder in binary array
+    binaryNum[i] = errorNumber % 2;
+    errorNumber = errorNumber / 2;
+    i++;
+  }
+
+  // printing binary array in reverse order
+  for (int j = i - 1; j >= 0; j--)
+    if (binaryNum[j] == 0) {
+      leds[j] = CRGB::Blue;
+    } else {
+      leds[j] = CRGB::Red;
+    }
 }
